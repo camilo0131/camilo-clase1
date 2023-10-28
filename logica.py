c229a1e1faef07c64afec8da5aec5a7d9b03967e
camilo0131 <lopesgomescamilo0131@gmail.com>
2023-10-28 12:03:36 -0500

    calcular el area de un cuadrado

---
+++ logica.py
@@ -0,0 +1,10 @@
+number = 5
+
+print('El valor de la variable es:', number)
+
+base = int(input('ingrese la base'))
+altura = int(input('ingrese altura'))
+
+resultado = base * altura
+
+print('el area del cuadrado es:' , resultado)
